940ce7b36cab58f5007e31ca52ef6603a767746b
reb <reboumans@gmail.com>
2019-02-28 19:22:17 +0100

    Basic solver uses all possible photos

--- slideshows/solvers/basic.py
+++ slideshows/solvers/basic.py
@@ -2,8 +2,18 @@ import logging
 
 logger = logging.getLogger('solver/basic.py')
 
+def pairwise(iterable):
+    a = iter(iterable)
+    return zip(a, a)
 
 def solve(problem):
+    slideshow = []
+    verticals = []
     for photo in problem['photos']:
         if photo['orientation'] == 'H':
-            return [[photo['index']]]
+            slideshow.append([photo['index']])
+        else:
+            verticals.append(photo['index'])
+    for photo_a, photo_b in pairwise(verticals):
+        slideshow.append([photo_a, photo_b])
+    return slideshow
