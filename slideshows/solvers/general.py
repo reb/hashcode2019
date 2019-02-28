62541597b17693ca6edd41e21a3b253d93f09cc4
reb <reboumans@gmail.com>
2019-02-28 20:27:19 +0100

    Improve value function

--- slideshows/solvers/general.py
+++ slideshows/solvers/general.py
@@ -1,13 +1,5 @@
-from itertools import product
-
-
 def value(tags_a, tags_b):
-    same = set()
-    only_in_a = set()
-    only_in_b = set()
-    for tag_a, tag_b in product(tags_a, tags_b):
-        if tag_a == tag_b:
-            same.add(tag_a)
-        only_in_a.add(tag_a)
-        only_in_b.add(tag_b)
-    return min(len(same), len(only_in_a), len(only_in_b))
+    intersect_length = len(set(tags_a).intersection(tags_b))
+    only_in_a = len(tags_a) - intersect_length
+    only_in_b = len(tags_b) - intersect_length
+    return min(intersect_length, only_in_a, only_in_b)
--- slideshows/test/solvers/test_general.py
+++ slideshows/test/solvers/test_general.py
@@ -5,3 +5,9 @@ class TestValue(unittest.TestCase):
 
     def test_normal(self):
         self.assertEqual(general.value(['a', 'b'], ['b', 'c']), 1)
+
+    def test_same_ending(self):
+        self.assertEqual(general.value(['b', 'c', 'e'], ['d', 'e']), 1)
+
+    def test_bigger(self):
+        self.assertEqual(general.value(['a', 'b', 'c', 'e'], ['a', 'd', 'e', 'f']), 2)
